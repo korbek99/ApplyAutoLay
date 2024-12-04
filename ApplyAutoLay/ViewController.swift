171523570d43a7e3dfeb7978c6108cc86db0af4d
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2024-12-04 17:30:34 -0300

    Initial Commit

---
+++ ApplyAutoLay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplyAutoLay
+//
+//  Created by Jose Preatorian on 04-12-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
